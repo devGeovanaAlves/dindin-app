41696e77ce35de85831f0648c96668b7659d6eca
Geovana Alves <geovana.m.alves@hotmail.com>
2023-09-04 17:06:11 -0300

    feat: adicionados conteúdos de SignUpForm component

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,5 +1,44 @@
+import { Link } from "react-router-dom";
+import "../styles/SignUpForm.css";
+
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  return (
+    <div className="container-form sign-up">
+      <form>
+        <h1>Cadastre-se</h1>
+        <label>
+          <span>Nome</span>
+          <input type="text" name="name" />
+        </label>
+
+        <label>
+          <span>E-mail</span>
+          <input type="text" name="e-mail" />
+        </label>
+
+        <label>
+          <span>Senha</span>
+          <input type="password" name="password" />
+        </label>
+
+        <label>
+          <span>Confirmação de senha</span>
+          <input type="password" name="password" />
+        </label>
+
+        <input
+          className="input-submit"
+          type="submit"
+          name="sign-in"
+          value="Cadastrar"
+        />
+
+        <span className="redirect-home">
+          Já tem cadastro?<Link to="/"> Clique aqui!</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
