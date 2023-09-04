366941395e40d8f77285300692d1fccd8363a1a0
Geovana Alves <geovana.m.alves@hotmail.com>
2023-09-04 12:34:23 -0300

    feat: adicionados elementos de Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import Header from "../components/Header";
+import SignIn from "../components/SignInForm";
+import Description from "../components/DescriptionHome";
+import "../styles/Home.css";
+
 const Home = () => {
-  return <div>Página inicial</div>;
+  return (
+    <div className="container-page-home">
+      <Header />
+      <div className="container-main-home">
+        <SignIn />
+        <Description />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
