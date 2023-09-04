5bf47561cc90fe81a9ed8be49be65698e6a0591c
Geovana Alves <geovana.m.alves@hotmail.com>
2023-09-04 15:03:58 -0300

    feat: adicionados elementos em SignIn component

--- frontend/src/components/SignInForm.jsx
+++ frontend/src/components/SignInForm.jsx
@@ -1,5 +1,29 @@
+import "../styles/SignInForm.css";
+
 const SignInForm = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container-form-sign-in">
+      <form>
+        <h1>Login</h1>
+        <label>
+          <span>E-mail</span>
+          <input type="text" name="e-mail" />
+        </label>
+
+        <label>
+          <span>Password</span>
+          <input type="password" name="password" />
+        </label>
+
+        <input
+          className="input-submit"
+          type="submit"
+          name="sign-in"
+          value="Entrar"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SignInForm;
