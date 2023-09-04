1d3ab44eeb74967f9ba0732414557e588be184b7
Geovana Alves <geovana.m.alves@hotmail.com>
2023-09-04 13:10:42 -0300

    feat: adicionado container para informações de usuário

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,7 @@
 import Logo from "../../public/logo-icon.svg";
+import Avatar from "../assets/profile-icon.svg";
+import Logout from "../assets/logout-icon.svg";
+import "../styles/Header.css";
 
 const Header = () => {
   return (
@@ -7,6 +10,12 @@ const Header = () => {
         <img src={Logo} alt="Logotipo" />
         <span>Dindin</span>
       </div>
+
+      {/* <div className="container-user-info">
+        <img className="avatar-icon" src={Avatar} alt="Avatar" />
+        <span>Name</span>
+        <img className="logout-icon" src={Logout} alt="Logout" />
+      </div> */}
     </header>
   );
 };
