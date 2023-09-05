9e7c1fdbcca7a099b726cd16f116fc9ddaa0056f
Geovana Alves <geovana.m.alves@hotmail.com>
2023-09-04 21:23:29 -0300

    feat: adicionados componentes de Dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,19 @@
+import Header from "../components/HeaderDashboard";
+import TransactionsList from "../components/TransactionsList";
+import TransactionsResume from "../components/TransactionsResume";
+import "../styles/Dashboard.css";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="container-dashboard">
+      <Header />
+
+      <main className="container-transactions-components">
+        <TransactionsList />
+        <TransactionsResume />
+      </main>
+    </div>
+  );
 };
 
 export default Dashboard;
