6990b1a101c3ecb420fddf0623014779de1bc687
Geovana Alves <geovana.m.alves@hotmail.com>
2023-09-06 19:29:11 -0300

    feat: adicionado classes a elementos

--- frontend/src/components/AddTransactionModal.jsx
+++ frontend/src/components/AddTransactionModal.jsx
@@ -1,56 +1,59 @@
 import { Link } from "react-router-dom";
-import CloseIcon from "../assets/close-icon.svg";
+import "../styles/AddTransactionModal.css";
 
 const AddTransactionModal = () => {
   return (
     <div className="external-modal-container">
       <div className="internal-modal-container">
-        <div className="modal-container-title" />
-        <h1>Adicionar Registro</h1>
-        <Link>
-          <img src={CloseIcon} alt="Fechar" />
-        </Link>
+        <div className="modal-container-title">
+          <h1>Adicionar Registro</h1>
+          <Link></Link>
+        </div>
+
+        <div className="modal-container-buttons">
+          <button>Entrada</button>
+          <button>Saída</button>
+        </div>
+
+        <form>
+          <label>
+            <span>Valor</span>
+            <input className="input-modal" type="number" name="value" min="1" />
+          </label>
+
+          <label>
+            <span>Categoria</span>
+            <select className="input-modal" name="category">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Assinaturas e Serviços">
+                Assinaturas e Serviços
+              </option>
+              <option value="Casa">Casa</option>
+              <option value="Compras">Compras</option>
+              <option value="Cuidados pessoais">Cuidados pessoais</option>
+              <option value="Educação">Educação</option>
+            </select>
+          </label>
+
+          <label>
+            <span>Data</span>
+            <input className="input-modal" type="date" name="date" />
+          </label>
+
+          <label>
+            <span>Descrição</span>
+            <input className="input-modal" type="text" name="description" />
+          </label>
+
+          <label>
+            <input
+              className="input-submit-modal"
+              type="submit"
+              value="Confirmar"
+            />
+          </label>
+        </form>
       </div>
-
-      <div className="modal-container-buttons">
-        <button>Entrada</button>
-        <button>Saída</button>
-      </div>
-
-      <form>
-        <label>
-          <span>Valor</span>
-          <input type="number" name="value" min="1" />
-        </label>
-
-        <label>
-          <span>Categoria</span>
-          <select name="category">
-            <option value="Alimentação">Alimentação</option>
-            <option value="Assinaturas e Serviços">
-              Assinaturas e Serviços
-            </option>
-            <option value="Casa">Casa</option>
-            <option value="Compras">Compras</option>
-            <option value="Cuidados pessoais">Cuidados pessoais</option>
-            <option value="Educação">Educação</option>
-          </select>
-        </label>
-
-        <label>
-          <span>Data</span>
-          <input type="date" name="date" />
-        </label>
-
-        <label>
-          <span>Descrição</span>
-          <input type="text" name="description" />
-        </label>
-
-        <label>
-          <input type="submit" value="Confirmar" />
-        </label>
-      </form>
     </div>
   );
 };
