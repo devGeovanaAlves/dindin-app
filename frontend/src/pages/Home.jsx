1c0297cccc26e5c725742c32b5a6eaf228485106
Geovana Alves <geovana.m.alves@hotmail.com>
2023-09-13 12:22:42 -0300

    feat: adicionado lógica para rota protegida quando logado

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,88 @@
 import Header from "../components/Header";
-import SignIn from "../components/SignInForm";
+// import SignIn from "../components/SignInForm";
 import Description from "../components/DescriptionHome";
 import "../styles/Home.css";
+import "../styles/SignInForm.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Home = ({ handleDataAuth }) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [status, setStatus] = useState("");
+
+  const styleErrorMessage = {
+    color: "#ff576b",
+    fontSize: "14px",
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let storageUser = localStorage.getItem(`${email}`);
+    let user = JSON.parse(storageUser);
+
+    handleDataAuth({ userPass: user.password, formPass: password });
+
+    if (!storageUser) {
+      setError(true);
+      setStatus("Usuário não encontrado");
+      return;
+    }
+
+    if (user.password === password) {
+      navigate("/dashboard");
+    } else {
+      setError(true);
+      setStatus("Senha incorreta");
+      return;
+    }
+
+    setError(false);
+  };
+
   return (
     <div className="container-page home">
       <Header />
       <div className="container-main-home">
         <Description />
-        <SignIn />
+        <div className="container-form sign-in">
+          <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <label>
+              <span>E-mail</span>
+              <input
+                type="text"
+                name="e-mail"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                required
+              />
+            </label>
+
+            <label>
+              <span>Password</span>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                required
+              />
+            </label>
+
+            {error && <span style={styleErrorMessage}>{status}</span>}
+
+            <input
+              className="input-submit"
+              type="submit"
+              name="sign-in"
+              value="Entrar"
+            />
+          </form>
+        </div>
       </div>
     </div>
   );
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import "./styles/global.css";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
@@ -7,17 +6,51 @@ import Dashboard from "./pages/Dashboard";
 import EditTransaction from "./components/EditTransactionModal";
 import AddTransaction from "./components/AddTransactionModal";
 import EditProfile from "./components/EditProfileModal";
+import { useEffect, useState } from "react";
 
 const MainRoutes = () => {
+  const [dataAuth, setDataAuth] = useState({});
+
+  const handleDataAuth = (childObj) => {
+    setDataAuth(childObj);
+  };
+
+  const isAuthenticated = ({ userPass, formPass }) => {
+    return userPass && formPass && userPass === formPass ? true : false;
+  };
+
+  const ProtectedRoutes = ({ condition, redirectTo }) => {
+    return condition ? <Outlet /> : <Navigate to={redirectTo} />;
+  };
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/edit/:id" element={<EditTransaction />} />
-        <Route path="/dashboard/add" element={<AddTransaction />} />
-        <Route path="/dashboard/edit-profile" element={<EditProfile />} />
+        <Route
+          element={
+            <ProtectedRoutes
+              condition={!isAuthenticated(dataAuth)}
+              redirectTo={"/dashboard"}
+            />
+          }
+        >
+          <Route path="/" element={<Home handleDataAuth={handleDataAuth} />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+
+        <Route
+          element={
+            <ProtectedRoutes
+              condition={isAuthenticated(dataAuth)}
+              redirectTo={"/"}
+            />
+          }
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/edit/:id" element={<EditTransaction />} />
+          <Route path="/dashboard/add" element={<AddTransaction />} />
+          <Route path="/dashboard/edit-profile" element={<EditProfile />} />
+        </Route>
       </Routes>
     </>
   );
