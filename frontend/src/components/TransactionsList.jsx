8a044f027f3c1999f8a20087811d2a52b55f878f
Geovana Souza <129792556+devGeovanaAlves@users.noreply.github.com>
2023-09-15 19:40:55 -0300

    style: adicionado scroll em lista de transações

--- frontend/src/components/TransactionsList.jsx
+++ frontend/src/components/TransactionsList.jsx
@@ -37,60 +37,62 @@ const TransactionsList = ({
 
     catchDataTransaction(data);
     setToGoEdit(true);
-    setShowModal(true); //alterado
+    setShowModal(true);
     setModalType("Editar");
   };
 
   return (
-    <table className="table-transactions-list">
-      <thead className="container-header-table">
-        <tr>
-          <th className=" header-column">Data</th>
-          <th className=" header-column">Dia da semana</th>
-          <th className=" header-column">Descrição</th>
-          <th className=" header-column">Categoria</th>
-          <th className=" header-column">Valor</th>
-          <th className=" header-column"></th>
-        </tr>
-      </thead>
+    <div className="wrapper-table">
+      <table className="table-transactions-list">
+        <thead className="container-header-table">
+          <tr>
+            <th className=" header-column">Data</th>
+            <th className=" header-column">Dia da semana</th>
+            <th className=" header-column">Descrição</th>
+            <th className=" header-column">Categoria</th>
+            <th className=" header-column">Valor</th>
+            <th className=" header-column"></th>
+          </tr>
+        </thead>
 
-      <tbody className="container-body-table">
-        {transactions.map((transaction) => {
-          let colorValue =
-            transaction.type === "entrada"
-              ? { color: "#7B61FF", fontWeight: "700" }
-              : { color: "#FA8C10", fontWeight: "700" };
-          return (
-            <tr key={transaction.id} className="container-line">
-              <td className="line-date">{transaction.dateFormat}</td>
-              <td>{transaction.day} </td>
-              <td>{transaction.description} </td>
-              <td>{transaction.category} </td>
-              <td style={colorValue}>
-                {toCurrencyStyle(parseInt(transaction.value))}{" "}
-              </td>
-              <td className="buttons-line">
-                <button onClick={() => handleEdit(transaction)}>
-                  <img
-                    className="edit-icon"
-                    src={EditIcon}
-                    alt="Editar registro"
-                  />
-                </button>
+        <tbody className="container-body-table">
+          {transactions.map((transaction) => {
+            let colorValue =
+              transaction.type === "entrada"
+                ? { color: "#7B61FF", fontWeight: "700" }
+                : { color: "#FA8C10", fontWeight: "700" };
+            return (
+              <tr key={transaction.id} className="container-line">
+                <td className="line-date">{transaction.dateFormat}</td>
+                <td>{transaction.day} </td>
+                <td>{transaction.description} </td>
+                <td>{transaction.category} </td>
+                <td style={colorValue}>
+                  {toCurrencyStyle(parseInt(transaction.value))}{" "}
+                </td>
+                <td className="buttons-line">
+                  <button onClick={() => handleEdit(transaction)}>
+                    <img
+                      className="edit-icon"
+                      src={EditIcon}
+                      alt="Editar registro"
+                    />
+                  </button>
 
-                <button onClick={() => remove(transaction)}>
-                  <img
-                    className="remove-icon"
-                    src={RemoveIcon}
-                    alt="Excluir registro"
-                  />
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                  <button onClick={() => remove(transaction)}>
+                    <img
+                      className="remove-icon"
+                      src={RemoveIcon}
+                      alt="Excluir registro"
+                    />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
